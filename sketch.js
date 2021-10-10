713e6dd9959aba5293d3b1c4b57aee3df1455032
ikilol <87668373+ikilol@users.noreply.github.com>
2021-10-10 22:46:38 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,34 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  angleMode(DEGREES);
+  background("#caf0f8");
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  noFill();
+
+  let myColor = lerpColor(color("#03045E"), color("#CAF0F8"), frameCount / 120);
+
+  stroke(myColor);
+  translate(0, frameCount * 3);
+  circle(-200, 100, 150);
+  circle(100, 100, 150);
+  circle(400, 100, 150);
+  circle(700, 100, 150);
+  circle(1000, 100, 150);
+  circle(1300, 100, 150);
+  circle(1600, 100, 150);
+
+  translate(0, -frameCount * 6);
+  circle(250, 600, 150);
+  circle(550, 600, 150);
+  circle(850, 600, 150);
+  circle(1150, 600, 150);
+  circle(1450, 600, 150);
+  circle(1750, 600, 150);
+  circle(2050, 600, 150);
+
+  if (frameCount > 120) {
+    noLoop();
+  }
 }
